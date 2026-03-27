19b1d6866f6c6ca99198d9cb192a73135c696462
ogustavo14 <121241931+ogustavo14@users.noreply.github.com>
2026-03-27 14:11:49 +0000

    finish endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAT = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "Show max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnetionsResult = await database.query({
+    text: "SElECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  // "SElECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  const databaseValue = databaseOpenConnetionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAT,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseValue,
+      },
+    },
+  });
 }
 
 export default status;
